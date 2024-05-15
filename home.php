dbf439a1c208f69b9e98103f9618ae7f017e3ffc
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-05-15 14:37:13 +0200

    add permalink in posts

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 
 	if ($query->have_posts()) :
 		while ($query->have_posts()) : $query->the_post(); ?>
-			<div class="card">
+			<a href="<?php the_permalink(); ?>" class="card">
 				<div class="meta">
 					<h5 class="date"><?php echo get_the_date('j F Y'); ?></h5>
 					<?php the_title('<h2 class="post-title">', '</h2>'); ?>
@@ -21,7 +21,7 @@
 					</span>
 				</div>			
 				<div class="thumbnail"><?php the_post_thumbnail('large'); ?></div>
-			</div>
+			</a>
 		<?php endwhile;
 	endif; 
 	wp_reset_postdata(); ?>
