da9ff65e7fa011c424f9ddfa573ed5b2ea1ec6af
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-05-15 19:06:43 +0200

    clean code

--- functions/utils.php
+++ functions/utils.php
@@ -1,11 +1,4 @@
 <?php
-function hide_taxonomy_description() {
-    global $taxnow;
-    if ($taxnow === 'frame-type' || $taxnow === 'wheel-size') {
-        echo '<style>.term-description-wrap { display: none; }</style>';
-    }
-}
-
 function custom_manage_frame_columns($columns) {
     unset($columns['date']);
     return $columns;
